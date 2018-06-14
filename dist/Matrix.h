071e3085d34b421371c86628fcdfef554e68e50e
Amir Forsati <pwwiur@yahoo.com>
2018-06-14 18:56:28 +0430

    Update Matrix.h

--- dist/Matrix.h
+++ dist/Matrix.h
@@ -1,19 +1,20 @@
 #ifndef Matrix_H
 #define Matrix_H
 #include <iostream>
-#include <string>
+#include <sstream>
+#include<math.h>
+
 using namespace std;
 
 template<class T> class Matrix{
     private:
         T* arr;
-
-        bool checkXY(int x, int y);
-        void error(int err = -1, string details = "");
         class Proxy {
             public:
-                Proxy(T* _arr, int width, int x);
-                T& operator[](short int y);
+                Proxy(T* _arr, int width, int x) : _arr(_arr), width(width), x(x){}
+                T& operator[](int y){
+                    return _arr[y + width * x];
+                }
             private:
                 T* _arr;
                 int width, x;
@@ -21,173 +22,331 @@ template<class T> class Matrix{
         class Dump{
             private:
                 Matrix* m;
+                template<class V> string toString(V var){
+                    ostringstream sstr;
+                    sstr << var;
+                    return sstr.str();
+                }
             public:
-                Dump(Matrix* m);
-                void array();
-                void table();
-                void matrix();
+                Dump(Matrix* m) : m(m){}
+                void array(){
+                    cout << "Array view: " << endl;
+                    for(int i = 0; i < m->width * m->height; i++){
+                        cout << "arr[" << i << "]: " << m->arr[i] << endl;
+                    }
+                }
+                void table(){
+                        cout << endl << "Table view: " << endl;
+                        int column[m->width];
+                        int length = 0;
+                        for(int i = 0; i < m->width; i++){
+                            cout << m->arr[i] << "            ";
+                            column[i] = toString(m->arr[i]).length() + 12;
+                            length += column[i];
+                        }
+                        cout << endl;
+                        for(int i = 0; i < length; i++){
+                            cout << "-";
+                        }
+                        cout << endl;
+                        for(int i = m->width; i < m->width * m->height; i++){
+                            int border = toString(m->arr[i]).length();
+                            bool etc = false;
+                            if(border > column[i % m->width])
+                                etc = true;
+
+                            for(int j = 0; j < column[i % m->width]; j++){
+                                if(j > border){
+                                    cout << " ";
+                                }
+                                else{
+                                    if(j < (column[i % m->width] - (etc ? 3 : 0)))
+                                        cout << toString(m->arr[i])[j];
+                                    else
+                                        cout << ".";
+                                }
+                            }
+                            cout << " ";
+                            if((i + 1) % m->width == 0)
+                                cout << endl;
+                        }
+
+                }
+                void matrix(){
+                    cout << endl << "Matrix view: " << endl;
+                    int column[m->width];
+                    int length = 0;
+                    int space;
+                    for(int i = 0; i < m->width; i++){
+                        space = sizeof(m->arr[i]);
+                        column[i] = toString(m->arr[i]).length() + (space > 12 ? space / 2 : space);
+                        length += column[i];
+                    }
+                    cout << "-";
+                    for (int i = 0; i < length + 2; i++){
+                        cout << " ";
+                    }
+                    cout << "-";
+                    cout << endl;
+                    for(int i = 0; i < m->width * m->height; i++){
+                        if(i % m->width == 0)
+                            cout << "| ";
+                        int border = toString(m->arr[i]).length();
+                        bool etc = false;
+                        if(border > column[i % m->width])
+                            etc = true;
+
+                        for(int j = 0; j < column[i % m->width]; j++){
+                            if(j > border){
+                                cout << " ";
+                            }
+                            else{
+                                if(j < (column[i % m->width] - (etc ? 4 : 0)))
+                                    cout << toString(m->arr[i])[j];
+                                else{
+                                    cout << "...";
+                                    break;
+                                }
+                            }
+                        }
+                        cout << " ";
+                        if((i + 1) % m->width == 0){
+                            cout << " |";
+                            cout << endl;
+                        }
+                    }
+                    cout << "-";
+                    for (int i = 0; i < length + 2; i++){
+                        cout << " ";
+                    }
+                    cout << "-";
+                    cout << endl;
+                }
         };
 
     public:
         int width, height;
         Dump dump = Dump(this);
 
-        Matrix(int x, int y);
-        Proxy operator[](int x);
-        T& operator() (T key, int n = 1);
-};
-
-
-
-
-
-//-- Template class implementation
-template<class T> Matrix<T>::Matrix(int width, int height){
-    arr = new T[width * height];
-    this->width = width;
-    this->height = height;
-}
-
-template<class T> void Matrix<T>::error(int err, string details){
-    string errText;
-    switch(err){
-        case 0:
-            errText = "Error while searching '" + details + "'";
-            break;
-        case -1:
-        default:
-            errText = "An error has been occured with matrix";
-    }
-    cout << errText;
-    exit(EXIT_FAILURE);
-}
-
-template<class T> T& Matrix<T>::operator() (T key, int n) {
-    for(int i = 0; i < width; i++){
-        if(arr[i] == key)
-            return arr[i + width * n];
-    }
-    error(0, key);
-}
-
-template<class T> bool Matrix<T>::checkXY(int x, int y){
-    return x >= 0 && x < width && y >= 0 && y < height;
-}
-
-
-template<class T> Matrix<T>::Proxy::Proxy(T* _arr, int width, int x) : _arr(_arr), width(width), x(x){}
-
-template<class T> T& Matrix<T>::Proxy::operator[](short int y){
-    return _arr[y+width*x];
-}
-
-template<class T> typename Matrix<T>::Proxy Matrix<T>::operator[](int x){
-    return Proxy(arr, width, x);
-}
-
-
-
-template<class T> Matrix<T>::Dump::Dump(Matrix* m) : m(m){}
-
-template<class T> void Matrix<T>::Dump::array(){
-    cout << "Array view: " << endl;
-    for(int i = 0; i < m->width * m->height; i++){
-        cout << "arr[" << i << "]: " << m->arr[i] << endl;
-    }
-}
-
-template<class T> void Matrix<T>::Dump::table(){
-    cout << endl << "Table view: " << endl;
-    int column[m->width];
-    int length = 0;
-    for(int i = 0; i < m->width; i++){
-        cout << m->arr[i] << "            ";;
-        column[i] = m->arr[i].length() + 12;
-        length += column[i];
-    }
-    cout << endl;
-    for(int i = 0; i < length; i++){
-        cout << "-";
-    }
-    cout << endl;
-    for(int i = m->width; i < m->width * m->height; i++){
-        int border = m->arr[i].length();
-        bool etc = false;
-        if(border > column[i % 3])
-            etc = true;
-
-        for(int j = 0; j < column[i % 3]; j++){
-            if(j > border){
-                cout << " ";
-            }
-            else{
-                if(j < (column[i % 3] - (etc ? 3 : 0)))
-                    cout << m->arr[i][j];
-                else
-                    cout << ".";
-            }
+        Matrix(int rows, int columns): width(columns), height(rows){
+            arr = new T[rows * columns];
         }
-        cout << " ";
-        if((i + 1) % m->width == 0)
-            cout << endl;
-    }
-}
-
-template<class T> void Matrix<T>::Dump::matrix(){
-    cout << endl << "Matrix view: " << endl;
-    int column[m->width];
-    int length = 0;
-    for(int i = 0; i < m->width; i++){
-        column[i] = m->arr[i].length() + 12;
-        length += column[i];
-    }
-    cout << "-";
-    for (int i = 0; i < length + 2; i++){
-        cout << " ";
-    }
-    cout << "-";
-    cout << endl;
-    cout << "|";
-    cout << " ";
-    for(int i = 0; i < m->width; i++){
-        cout << m->arr[i] << "            ";
-    }
-    cout << " ";
-    cout << "|";
-    cout << endl;
-    for(int i = m->width; i < m->width * m->height; i++){
-        if(i % m->width == 0)
-            cout << "| ";
-        int border = m->arr[i].length();
-        bool etc = false;
-        if(border > column[i % 3])
-            etc = true;
-
-        for(int j = 0; j < column[i % 3]; j++){
-            if(j > border){
-                cout << " ";
+        Proxy operator[] (int x){
+            return Proxy(arr, width, x);
+        }
+        T& operator() (T key, int n = 1){
+            for(int i = 0; i < width; i++){
+                if(arr[i] == key)
+                    return arr[i + width * n];
             }
-            else{
-                if(j < (column[i % 3] - (etc ? 4 : 0)))
-                    cout << m->arr[i][j];
-                else{
-                    cout << "...";
-                    break;
+            throw "Error: The key '" + key + "' was not fount in the matrix.";
+        }
+        bool operator== (Matrix m){
+            if(this->width == m.width && this->height == m.height){
+                for(int i = 0; i < this->width; i++){
+                    for(int j = 0; j < this->height; j++){
+                        if((*this)[i][j] != m[i][j])
+                            return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+        bool operator!= (Matrix m){
+            return !((*this) == m);
+        }
+        bool operator< (Matrix m){
+            if(this->width == m.width && this->height == m.height){
+                for(int i = 0; i < this->width; i++){
+                    for(int j = 0; j < this->height; j++){
+                        if((*this)[i][j] >= m[i][j])
+                            return false;
+                    }
+                }
+                return true;
+            }
+            else if(this->width *this->height < m.width * m.height){
+                return true;
+            }
+            return false;
+        }
+        bool operator> (Matrix m){
+            return !((*this) < m) && (*this) != m;
+        }
+        bool operator<= (Matrix m){
+            return (*this) < m || (*this) == m;
+        }
+        bool operator>= (Matrix m){
+            return (*this) > m || (*this) == m;
+        }
+        template<class S> Matrix<S> operator= (S x){
+            for(int i = 0; i < this->width; i++){
+                for(int j = 0; j < this->height; j++){
+                    if(x == 1 && i != j)
+                        continue;
+                    (*this)[i][j] = x;
                 }
             }
+            return (*this);
         }
-        cout << " ";
-        if((i + 1) % m->width == 0){
-            cout << " |";
-            cout << endl;
+        Matrix operator= (Matrix m){
+            this->arr = m.arr;
+            this->width = m.width;
+            this->height = m.height;
+            return *this;
+        }
+        Matrix operator+ (Matrix m){
+            Matrix result(this->width, this->height);
+            for(int i = 0; i < this->width; i++){
+                for(int j = 0; j < this->height; j++){
+                    result[i][j] = (*this)[i][j] + m[i][j];
+                }
+            }
+            return result;
+        }
+        template<class S> Matrix operator* (S x){
+            Matrix result(this->width, this->height);
+            for(int i = 0; i < this->width; i++){
+                for(int j = 0; j < this->height; j++){
+                    result[i][j] = (*this)[i][j] * x;
+                }
+            }
+            return result;
+        }
+        Matrix operator* (Matrix m){
+            if(this->width == m.height){
+                Matrix result(this->height, m.width);
+                for(int i = 0; i < this->height; i++){
+                    for(int j = 0; j < m.width; j++){
+                        for(int k = 0; k < this->width; k++){
+                            result[i][j] += (*this)[i][k] * m[k][j];
+                        }
+                    }
+                }
+                return result;
+            }
+            throw "Error: The height of first matrix should be equal to the width of the second matrix in multiplication.\n";
+        }
+        template<class S> Matrix operator/ (S x){
+            Matrix result(this->width, this->height);
+            for(int i = 0; i < this->width; i++){
+                for(int j = 0; j < this->height; j++){
+                    result[i][j] = (*this)[i][j] / x;
+                }
+            }
+            return result;
+        }
+        Matrix operator/ (Matrix m){
+            if(this->width == m.height && m.width == m.height){
+                Matrix result(this->height, m.width);
+                result = (*this) * (m ^ -1);
+                return result;
+            }
+            throw "Error: The height of first matrix should be equal to the width of the second matrix and second matrix should be square in derivation.\n";
+        }
+        Matrix operator^ (int x){
+            if(this->height == this->width){
+                int n = this->width;
+                Matrix result(n, n);
+
+                if(x < 0){
+                    Matrix inverse(n, n);
+                    inverse = cofactor(*this) * (1/det(*this));
+                    result = inverse ^ (x * -1);
+                }
+                else if(x == 0){
+                    result = (T) 1;
+                }
+                else{
+                    result = (*this);
+                    for(int i = 0; i < x - 1; i++){
+                        result = result * (*this);
+                    }
+                }
+                return result;
+            }
+            else{
+                throw "A matrix can raised to a certain power only if it has the same height and width length.\n";
+            }
+        }
+
+
+        void set(int x, int y, T value){
+            arr[y + width * x] = value;
+        }
+
+        void cset(int x, int y, char value){
+            arr[y + width * x] += value;
+        }
+
+        T get(int x, int y){
+            return arr[y + width * x];
+        }
+};
+template<class T> double det(Matrix<T> mat){
+    int n = mat.width;
+    double d; //det m
+    if(n == 1){
+        return mat[0][0];
+    }
+    if (n == 2){
+        return ((mat[0][0] * mat[1][1]) - (mat[1][0] * mat[0][1]));
+    }
+    else{
+        Matrix<T> submat(n - 1, n - 1);
+        for (int c = 0; c < n; c++){
+            int subi = 0; //submatrix's i value
+            for (int i = 1; i < n; i++){
+                int subj = 0;
+                for (int j = 0; j < n; j++){
+                    if (j == c)
+                        continue;
+                    submat[subi][subj] = mat[i][j];
+                    subj++;
+                }
+                subi++;
+            }
+            d += (pow(-1, c) * mat[0][c] * det(submat));
         }
     }
-    cout << "-";
-    for (int i = 0; i < length + 2; i++){
-        cout << " ";
+    return d;
+}
+// to calculate cofactor should caluculate minor then cofactor: https://en.wikipedia.org/wiki/Minor_(linear_algebra)
+template<class T> double Minor(Matrix<T> mat, int x, int y){
+    int n = mat.width;
+    double d; //minor m
+    Matrix<T> submat(n - 1, n - 1);
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(i == x or j == y)
+                continue;
+            submat[i > x ? i - 1 : i][j > y ? j - 1 : j] = mat[i][j];
+        }
     }
-    cout << "-";
-    cout << endl;
+    return det(submat);
 }
 
+template<class T> double cofactor(Matrix<T> mat, int x, int y){
+    return pow(-1, x + y) * (mat.width == 2 && x + y ==1 ? mat[x][y] : Minor(mat, x, y));
+}
+template<class T> Matrix<T> cofactor(Matrix<T> mat){
+    int n = mat.width;
+    Matrix<T> submat(n, n);
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            submat[i][j] = cofactor(mat, i, j);
+        }
+    }
+    return submat;
+}
+/*
+    Todo:
+    - Complete error exceptions
+    - Adding string indexes by brackets to associeted arrays
+    - Importing global functions to class object
+    - Optimizations of global functions and class members
+    - Data type support tests and improvements
+*/
 #endif // Matrix_H
